fc8be851c9eb90a0f3e408cf79e6d68f215d10ea
Imamah Zafar <imamah@Imamahs-MacBook-Pro.local>
2022-07-16 22:01:53 +0500

    Add week 1 content

--- objectDestructuring.js
+++ objectDestructuring.js
@@ -15,23 +15,23 @@ const person = {
   
   
   
-  // destructing
+  // destructuring
   const {person_name,gender} = person;
   console.log("person_name: "+person_name + " gender: "+ gender);
   
-  // destructing with default value 
+  // destructuring with default value 
   const {age = 60} = person;
   console.log("age: "+ age);
   
-  // destructing with aliases - address is not a variable
+  // destructuring with aliases - address is not a variable
   const {address: permenantAddress} = person;
   console.log("permenantAddress: "+permenantAddress);
   
-  // destructing with nested objects 
+  // destructuring with nested objects 
   const {department: {department_address}} = person;
   console.log(department_address);
   
-  // destructing in function params 
+  // destructuring in function params 
   const logUser = ({person_name, age}) => {
     console.log(`Person's name is: ${person_name} and age is: ${age}`);
   }
